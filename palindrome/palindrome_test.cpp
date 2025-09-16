527eb4d73916fe880808f6c0a3cdb2ec7386888b
Jaden Rainey <raine123@d.umn.edu>
2025-09-16 14:55:59 -0500

    palindrome test passing

--- palindrome/palindrome_test.cpp
+++ palindrome/palindrome_test.cpp
@@ -3,11 +3,15 @@
 #include "palindrome.h"
 
 void is_palindrome(char const *str) {
-  ASSERT_STREQ(palindrome(str), "Yes");
+  char* result = palindrome(str);
+  ASSERT_STREQ("Yes", result);
+  free(result);
 }
 
 void not_palindrome(char const *str) {
-  ASSERT_STREQ(palindrome(str), "No");
+  char* result = palindrome(str);
+  ASSERT_STREQ("No", result);
+  free(result);
 }
 
 TEST(Palindrome, HandlesEmptyString) {
